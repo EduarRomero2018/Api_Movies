59abcdd86779829872274b46eb2b3248b5c8897b
Eduar Romero <el.g21@hotmail.com>
2023-10-15 23:45:40 -0500

    Código de función upcomingMovies (Continuación del commit anterior)

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,10 @@
 // importamos el EndPoint a utilizar
-import { popularMovies } from "./endPoints.js";
+import { popularMovies, upcomingMoviesAll } from "./endPoints.js";
 import { image } from "./endPoints.js";
 import { cards_container, siguiente, anterior } from "../Components/nodos.js";
+// import { presionaste} from "../Pages/categoriesMovies.js";
+
+// presionaste();
 
 const complemens = '&page='; //EndPoints para la paginación
 
@@ -42,7 +45,7 @@ const fetch_API = async () => {
         // Validamos los diferentes estado que puede tener la respuesta
         if (respuesta.status === 200) {
             const datos = await respuesta.json();
-            console.log(datos);
+            // console.log(datos); ESTEEEEEEE
             let peliculas = '';
             // console.log('Cantidad de peliculas por pagina -> ' + datos.results.length);
             let allListMovie = datos.total_pages;
@@ -82,8 +85,56 @@ const fetch_API = async () => {
     }
 }
 
+const upcomingMovies = async () => {
+    try {
+        const respuesta = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=285183e62c53d84f3d35c88747b0ab65');
+
+        if (respuesta.status === 200) { //validamos el codigo de estado de la peticion
+            const datos = await respuesta.json();
+            const upcomingMoviesList = datos.results; //Guardamos el objeto en la variable
+
+            const todayDate = new Date().toLocaleDateString().split('/').reverse().join('-'); // Formateamos la fecha actual a como viene en la API para poder hacer luego la comparación
+            const filteredMovies = upcomingMoviesList.filter(element => element.release_date > todayDate);// Filtramos las películas que cumplan con la condicion
+
+            if (filteredMovies.length > 0) {
+                console.log('Películas próximas lanzadas:', filteredMovies);
+                const contenMoviereleases = document.querySelector('.movieReleases');
+                contenMoviereleases.innerHTML = filteredMovies.map(element =>
+                    `
+                    <div class="movie-container card">
+                        <img src="https://image.tmdb.org/t/p/w300/${element.poster_path}" class="img_size" alt="Image">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item date"> <b>Fecha de Lanzamiento -> </b>${element.release_date}</li>
+                            <li class="list-group-item"> <b>Nivel de Puntuación -> </b>${element.vote_average}</li>
+                            <!--<li class="list-group-item"> <b>Nivel de Puntuación -> </b>${element.overview}</li>-->
+                        </ul>
+                    </div>
+                    `
+                );
+
+
+            } else {
+                console.log('No hay películas próximas lanzadas.');
+            }
+        } else if (respuesta.status === 401) {
+            console.log('Autenticación fallida, usted no cuenta con el permiso para acceder a este servicio');
+        } else if (respuesta.status === 404) {
+            console.log('Películas No Encontradas');
+        } else {
+            console.log('Hubo un error inesperado, favor validar');
+        }
+    } catch (error) {
+        console.log('Error', error);
+    }
+}
+
+
+upcomingMovies();
+
+
+
 // Exportación de funciones a utilizar
 export {
     fetch_API,
-    // allListMovie,
+
 }
